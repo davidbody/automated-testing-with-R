a3fff768dd5ae622b5bc9e7eef0240c98155cef1
David W. Body <davidbody@bigcreek.com>
2016-06-18 15:13:54 -0500

    Documentation and code formatting

--- accuracy.R
+++ accuracy.R
@@ -1,4 +1,9 @@
+## Compute accuracy measures from predicted and actual
+## binary classification values. Parameters `predicted`
+## and `actual` are vectors of logical (T/F) values.
+
 accuracy_measures <- function(predicted, actual) {
+
     confusion_matrix <- t(table(predicted, actual))
 
     TN <- confusion_matrix[1, 1]
@@ -6,18 +11,18 @@ accuracy_measures <- function(predicted, actual) {
     FN <- confusion_matrix[2, 1]
     TP <- confusion_matrix[2, 2]
 
-    accuracy <- (TN + TP) / (TN + FP + FN + TP)
-    precision <- TP / (TP + FP)
-    recall <- TP / (TP + FN)
+    accuracy    <- (TN + TP) / (TN + FP + FN + TP)
+    precision   <- TP / (TP + FP)
+    recall      <- TP / (TP + FN)
     sensitivity <- TP / (TP + FN)
     specificity <- TN / (TN + FP)
-    F1 <- (2 * precision * recall) / (precision + recall)
+    F1          <- (2 * precision * recall) / (precision + recall)
 
     list(confusion_matrix = confusion_matrix,
-         accuracy = accuracy,
-         precision = precision,
-         recall = recall,
-         sensitivity = sensitivity,
-         specificity = specificity,
-         F1 = F1)
+         accuracy         = accuracy,
+         precision        = precision,
+         recall           = recall,
+         sensitivity      = sensitivity,
+         specificity      = specificity,
+         F1               = F1)
 }
