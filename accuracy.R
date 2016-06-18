c995d9ce0bb041ef7a6cc832fde8e3d962449294
David W. Body <davidbody@bigcreek.com>
2016-06-18 14:31:44 -0500

    Initial implementation of basic accuracy measures

--- accuracy.R
+++ accuracy.R
@@ -1,5 +1,23 @@
 accuracy_measures <- function(predicted, actual) {
-    ctable <- table(predicted, actual)
-    accuracy <- sum(diag(ctable)) / sum(ctable)
-    list(accuracy = accuracy)
+    confusion_matrix <- t(table(predicted, actual))
+
+    TN <- confusion_matrix[1, 1]
+    FP <- confusion_matrix[1, 2]
+    FN <- confusion_matrix[2, 1]
+    TP <- confusion_matrix[2, 2]
+
+    accuracy <- (TN + TP) / (TN + FP + FN + TP)
+    precision <- TP / (TP + FP)
+    recall <- TP / (TP + FN)
+    sensitivity <- TP / (TP + FN)
+    specificity <- TN / (TN + FP)
+    F1 <- (2 * precision * recall) / (precision + recall)
+
+    list(confusion_matrix = confusion_matrix,
+         accuracy = accuracy,
+         precision = precision,
+         recall = recall,
+         sensitivity = sensitivity,
+         specificity = specificity,
+         F1 = F1)
 }
--- test_accuarcy.R
+++ test_accuarcy.R
@@ -1,12 +1,47 @@
 library(testthat)
 
 source("accuracy.R")
+source("confusion.R")
 
 context("Binary classification")
 
-test_that("basic accuarcy measures", {
-    predicted <- c(T, T, F, F)
-    actual <- c(T, F, T, F)
+confusion_matrix <- make_table(c(264, 14,
+                                 22, 158))
+test_data <- inputs(confusion_matrix)
+predicted <- test_data$predicted
+actual <- test_data$actual
+
+test_that("confustion matrix is correct", {
     am <- accuracy_measures(predicted, actual)
-    expect_equal(am$accuracy, 0.5)
+    expect_equal(am$confusion_matrix, confusion_matrix)
+})
+
+test_that("accuarcy is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$accuracy, 0.92139738)
+})
+
+test_that("precision is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$precision, 0.918604651)
+})
+
+test_that("recall is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$recall, 0.877777778)
+})
+
+test_that("sensitivity is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$sensitivity, 0.877777778)
+})
+
+test_that("specificty is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$specificity, 0.949640288)
+})
+
+test_that("F1 is correct", {
+    am <- accuracy_measures(predicted, actual)
+    expect_equal(am$F1, 0.897727273)
 })
